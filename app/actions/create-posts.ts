9b53345944359462877aa9ad1fe5b55c7389f206
hasanb69 <hasanbaratiasp@gmail.com>
2024-03-01 11:40:21 +0330

    Starting create post function

--- app/actions/create-posts.ts
+++ app/actions/create-posts.ts
@@ -1,2 +1,17 @@
-"use server"
-export async function CreatePosts(){}
+"use server";
+
+import { z } from "zod";
+
+const createPostSchema = z.object({
+  title: z.string().min(3),
+  content: z.string().min(10),
+});
+interface CreateFormState{
+    errors:{
+        title?:string[],
+        content?:string[],
+        _form?:string[]
+
+    }
+}
+export async function CreatePosts() {}
--- app/topics/[slug]/page.tsx
+++ app/topics/[slug]/page.tsx
@@ -1,4 +1,18 @@
+import PostCreateForm from "@/app/components/posts/post-create-form"
 
-export default function TopicShowPage(){
-    
+interface TopicShowPageProps{
+    params:{
+        slug:string
+    }
+}
+export default function TopicShowPage({params}:TopicShowPageProps){
+   const {slug}=params
+    return(
+        <div className="grid grid-cols-4 gap-4 p-4">
+          <div className="col-span-3">
+            <h1 className=" text-2xl font-bold mb-2">{slug}</h1>
+          </div>
+          <div><PostCreateForm/></div>
+        </div>
+    )
 }
